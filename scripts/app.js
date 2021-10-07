e5e21946ca104536040efd22daf4b7ffc0dcb5c9
Gayatri R <gayatrirajgor@gmail.com>
2021-10-07 17:30:22 +0100

    added key function

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,7 @@ function init(){
     for (let i = 0; i < squareCount; i++){
       const square = document.createElement('div')
       // console.log('SQUARE:', square)
-      square.innerText = i
+      // square.innerText = i
       grid.appendChild(square)
       squares.push(square)
     }
@@ -28,15 +28,34 @@ function init(){
     // console.log('square[marioPosition]', squares[marioPosition])
     squares[marioPosition].classList.add(marioClass)
   }
+
   function removeMario(position){
     squares[position].classList.remove(marioClass)
+  }
+  function handleKeyUp(event){
+    const key = event.keyCode
+    removeMario(marioCurrentPosition)
+
+    if (key === 39){
+      marioCurrentPosition++
+    } else if (key === 37){
+      marioCurrentPosition--
+    } else if (key === 38 && marioCurrentPosition >= width){
+      marioCurrentPosition -= width
+    } else if (key === 40 && marioCurrentPosition + width <= width * width - 1){
+      marioCurrentPosition += width
+    } else {
+      console.log('INVALID KEY PRESSED')
+    }
+
+    addMario(marioCurrentPosition)
   }
   function addCoin(){
     
   }
 
 
-
+  document.addEventListener('keyup', handleKeyUp)
   createGrid()
 
 }
