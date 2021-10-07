8ebab350081133c2fe91a062621a1a56c442d653
Gayatri R <gayatrirajgor@gmail.com>
2021-10-07 17:18:18 +0100

    add and remove mario function in js

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,10 @@ function init(){
   const squareCount = width * width
   const squares = []
 
-
+  const coinClass = 'coin'
+  const marioClass = 'mario'
+  const marioStartingPosition = 32
+  let marioCurrentPosition = 32
 
   function createGrid(){
     for (let i = 0; i < squareCount; i++){
@@ -18,9 +21,20 @@ function init(){
       grid.appendChild(square)
       squares.push(square)
     }
+    addMario(marioStartingPosition)
   }
 
-  
+  function addMario(marioPosition){
+    // console.log('square[marioPosition]', squares[marioPosition])
+    squares[marioPosition].classList.add(marioClass)
+  }
+  function removeMario(position){
+    squares[position].classList.remove(marioClass)
+  }
+  function addCoin(){
+    
+  }
+
 
 
   createGrid()
