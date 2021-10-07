db3972abf053aa384cb3c34350eac9605eede573
Gayatri R <gayatrirajgor@gmail.com>
2021-10-07 17:07:02 +0100

    added grid with js and css

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,29 @@
 function init(){
   // console.log('JS Connected')
 
+  const grid = document.querySelector('.grid')
+  // console.log('grid:', grid)
+
+  const width = 10
+  const squareCount = width * width
+  const squares = []
+
+
+
+  function createGrid(){
+    for (let i = 0; i < squareCount; i++){
+      const square = document.createElement('div')
+      // console.log('SQUARE:', square)
+      square.innerText = i
+      grid.appendChild(square)
+      squares.push(square)
+    }
+  }
+
   
 
 
+  createGrid()
 
 }
 window.addEventListener('DOMContentLoaded', init)
